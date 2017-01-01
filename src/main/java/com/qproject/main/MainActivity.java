5edda23f98e77309f72d218a3c4d36de0bac3b84
chengxiang.peng <chengxiang.peng@qunar.com>
2017-01-01 17:34:10 +0800

    初次提交main模块代码

---
+++ src/main/java/com/qproject/main/MainActivity.java
@@ -0,0 +1,47 @@
+package com.qproject.main;
+
+import android.view.View;
+
+import com.qproject.common.QBaseActivity;
+import com.qproject.knowledge.KnowledgeActivity;
+import com.qproject.main.R;
+import com.qproject.test.TestActivity;
+import com.qproject.ui.UiActivity;
+import com.qproject.main.classloader.ClassLoaderActivity;
+import com.qproject.main.render.view.RenderActivity;
+import com.qproject.main.smartupdate.SmartUpdateActivity;
+import com.qproject.feature.FeatureActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends QBaseActivity {
+
+    public void toFeature(View view) {
+        toActivityByClass(FeatureActivity.class);
+    }
+
+    public void toKnowledge(View view) {
+        toActivityByClass(KnowledgeActivity.class);
+    }
+
+    public void toUi(View view) {
+        toActivityByClass(UiActivity.class);
+    }
+
+    public void toTest(View view) {
+        toActivityByClass(TestActivity.class);
+    }
+
+    public void toSmartUpdate(View view) {
+        toActivityByClass(SmartUpdateActivity.class);
+    }
+
+    public void toClassLoader(View view) {
+        toActivityByClass(ClassLoaderActivity.class);
+    }
+
+    public void toRender(View view) {
+        toActivityByClass(RenderActivity.class);
+    }
+}
