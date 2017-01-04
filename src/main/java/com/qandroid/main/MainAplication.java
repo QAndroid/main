659b3066d639c279d9fc271641cc51cc967a4085
chengxiang.peng <chengxiang.peng@qunar.com>
2017-01-04 20:28:23 +0800

    集成了leakcanary，检测fragment

--- src/main/java/com/qandroid/main/MainAplication.java
+++ src/main/java/com/qandroid/main/MainAplication.java
@@ -2,6 +2,8 @@ package com.qandroid.main;
 
 import android.app.Application;
 
+import com.qandroid.common.QBaseActivity;
+import com.qandroid.common.QBaseApplication;
 import com.squareup.leakcanary.LeakCanary;
 
 import org.xutils.x;
@@ -9,7 +11,7 @@ import org.xutils.x;
 /**
  * Created by chengxiang.peng on 2016/12/9.
  */
-public class QAplication extends Application{
+public class MainAplication extends QBaseApplication{
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,11 +19,5 @@ public class QAplication extends Application{
         //初始化XUtils框架
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 开启debug会影响性能
-
-        //初始化LeakCanary
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
-        LeakCanary.install(this);
     }
 }
