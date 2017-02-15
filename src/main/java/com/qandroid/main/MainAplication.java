2f15366015cf8c44c167f64f2c32629e02f9aa0e
chengxiang.peng <chengxiang.peng@qunar.com>
2017-02-15 18:10:42 +0800

    修复了leakcanary检测

--- src/main/java/com/qandroid/main/MainAplication.java
+++ src/main/java/com/qandroid/main/MainAplication.java
@@ -1,13 +1,15 @@
 package com.qandroid.main;
 
-import com.qandroid.common.QBaseApplication;
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
 
 import org.xutils.x;
 
 /**
  * Created by chengxiang.peng on 2016/12/9.
  */
-public class MainAplication extends QBaseApplication{
+public class MainAplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -15,5 +17,11 @@ public class MainAplication extends QBaseApplication{
         //初始化XUtils框架
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 开启debug会影响性能
+
+        //初始化LeakCanary
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
     }
 }
